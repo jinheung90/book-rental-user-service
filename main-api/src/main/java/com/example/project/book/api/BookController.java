e5995e7a3f848ac85c28d84cbc7e6b12e0f6e08c
jinheueng <jinheung90@gmail.com>
2024-07-26 21:20:58 +0900

    레디스 캐시 세팅 완료

--- main-api/src/main/java/com/example/project/book/api/BookController.java
+++ main-api/src/main/java/com/example/project/book/api/BookController.java
@@ -1,4 +1,28 @@
 package com.example.project.book.api;
 
+
+import com.example.project.book.dto.UserBookDto;
+import com.example.project.book.service.BookService;
+import com.example.project.jpa.RestPage;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
 public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/books")
+    public ResponseEntity<Page<UserBookDto>> getAllBooks(
+            @RequestParam(name = "page") int page,
+            @RequestParam(name = "size") int size
+    ) {
+        return ResponseEntity.ok(bookService.pageBooks(page, size));
+    }
 }
