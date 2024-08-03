a07a0634401807b803143748cc0dafd2addec2d5
jinheueng <jinheung90@gmail.com>
2024-08-03 23:00:35 +0900

    redis config

--- user-cache-core/src/main/java/com/example/project/config/RedisConfig.java
+++ user-cache-core/src/main/java/com/example/project/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -10,10 +11,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
