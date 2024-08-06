548796530b804e82877946633100ee43816170ad
jinheueng <jinheung90@gmail.com>
2024-08-07 04:36:19 +0900

    필요없는 componentscan 제거

--- book-core/src/main/java/com/example/project/config/QuerydslConfiguration.java
+++ book-core/src/main/java/com/example/project/config/QuerydslConfiguration.java
@@ -4,12 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan
 public class QuerydslConfiguration {
     @PersistenceContext
     private EntityManager entityManager;
