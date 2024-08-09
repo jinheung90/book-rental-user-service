76663a1361e4abaac5e335165ba3336fe8aa1822
jinheueng <jinheung90@gmail.com>
2024-08-09 22:30:29 +0900

    delete UserProfileCache

--- user-cache-core/src/main/java/com/example/project/config/UserRedisConfig.java
+++ user-cache-core/src/main/java/com/example/project/config/UserRedisConfig.java
@@ -1,6 +1,5 @@
 package com.example.project.config;
 
-import com.example.project.user.entity.UserProfileCache;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +11,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class UserRedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String host;
-    @Value("${spring.data.redis.port}")
-    private int port;
-
 
     @Bean
     public RedisTemplate<String, Object> redisTemplateForObject(LettuceConnectionFactory connectionFactory) {
@@ -26,17 +20,6 @@ public class UserRedisConfig {
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisTemplate<String, UserProfileCache> userProfileCacheRedisTemplate(LettuceConnectionFactory connectionFactory) {
-        RedisTemplate<String, UserProfileCache> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(UserProfileCache.class));
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(UserProfileCache.class));
         return redisTemplate;
     }
 }
