be61c34b6ac5055ac937c0eb2cae5e0d527ece5c
jinheueng <jinheung90@gmail.com>
2024-10-08 00:06:27 +0900

    디플로이 수정

--- main-api/deploy/dev-deploy.sh
+++ main-api/deploy/dev-deploy.sh
@@ -7,3 +7,5 @@ sudo fuser -k 8080/tcp
 STDOUT=/home/ec2-user/stdout.log
 STDERR=/home/ec2-user/stderr.log
 sudo nohup  java -jar -Dspring.profiles.active=prod /opt/deploy.jar 1>>$STDOUT 2>>$STDERR &
+
+sleep 150
